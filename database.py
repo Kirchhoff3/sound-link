09b3b037bcc5a1733f3eb3c67e479022d4f0d668
0-Kirchhoff <44092924+0-Kirchhoff@users.noreply.github.com>
2021-01-19 18:06:37 +0300

    first commit

---
+++ database.py
@@ -0,0 +1,42 @@
+
+
+#class Database:
+#
+ #   def __init__(self):
+#
+ #       self.movies = {}
+  #      self._last_movie_key = 0
+#
+ #   def add_movies(self, movie):
+#
+ #       self._last_movie_key += 1
+  #      self.movies[self._last_movie_key] = movie
+   #     return self._last_movie_key
+#
+ #   def delete(self, movie_key):
+#
+ #       if movie_key in self.movies:
+#
+ #           del self.movies[movie_key]    
+  #      
+   # def get_movie(self, movie_key):
+#
+ #       movie = self.movies.get(movie_key)
+  #      
+   #     if movie is None:
+    #        return None
+#
+ #       movie_ = Movie(movie.title, year=movie.year)
+  #      return movie_
+#
+ #   def get_movies(self,):
+#
+ #       movies = []
+  #      for movie_key, movie in self.movies.items():
+#
+ #           movie_ = Movie(movie.title, year=movie.year)
+  #          movies.append((movie_key, movie_))
+#
+ #       return movies    
+#
+
